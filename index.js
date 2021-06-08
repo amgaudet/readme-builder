15991980568c2bee7f893db04e58428f784083e5
Shreekz <alanmguadet@gmail.com>
2021-06-07 20:59:43 -0400

    added generateBadge function

--- index.js
+++ index.js
@@ -5,8 +5,26 @@ const { prompt } = require("inquirer");
 // TODO: Create an array of questions for user input Array of objects
 //const questions = ['What is the name of your project?', 'Please enter a brief description of your project.', 'What are the steps needed to install your project?', 'What is the usage for your project?', 'How may some one contribute?', 'What are the test instructions?', 'What license would you like to use?', 'Please enter your GitHub username', 'Please enter your email address'];
 
-// TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
+const generateBadge = ({license}) => {
+    let badgeId;
+    switch (license) {
+        case 'Apache 2.0': {
+            badgeId = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+            break;
+        }
+        case 'BSD': {
+            badgeId = '[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)';
+            break;
+        }
+        case 'MIT': {
+            badgeId = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+             break;
+        }
+        default: console.log('Error creating License badge');
+    };
+    return badgeId;
+}
+
 
 const getLicense = ({license, user}) => {
     let licenseCode;
@@ -88,6 +106,7 @@ const renderReadMe = (answers) => {
     const readMe = 
 `# ${answers.title}
 
+${generateBadge(answers)}
 ## Contents
 1. [Description](#Description)
 2. [Installation](#Installation)
@@ -131,46 +150,55 @@ function init() {
                 type: 'input',
                 message: 'What is your name',
                 name: 'user',
+                default: 'testentry'
             },
             {
                 type: 'input',
                 message: 'What is the name of your project?',
                 name: 'title',
+                default: 'testentry'
             },
             {
                 type: 'input',
                 message: 'Please enter a brief description of your project.',
                 name: 'description',
+                default: 'testentry'
             },
             {
                 type: 'input',
                 message: 'What are the steps needed to install your project?',
                 name: 'install',
+                default: 'testentry'
             },
             {
                 type: 'input',
                 message: 'What is the usage for your project?',
                 name: 'usage',
+                default: 'testentry'
             },
             {
                 type: 'input',
                 message: 'How may some one contribute?',
                 name: 'contribute',
+                default: 'testentry'
             },
             {
                 type: 'input',
                 message: 'What are the test instructions?',
                 name: 'test',
+                default: 'testentry'
             },
             {
                 type: 'input',
                 message: 'Please enter your GitHub username',
                 name: 'github',
+                default: 'testentry'
             },
             {
                 type: 'input',
                 message: 'Please enter your email address',
                 name: 'email',
+                default: 'testentry'
             },
             {
                 type: 'list',
